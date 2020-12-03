d2156c94727dbbe6f4ad362e31b14e0fb287fb84
tanzeel <tanzeelwahid111@gmail.com>
2020-12-03 17:10:04 +0500

    JS password generator

---
+++ app.js
@@ -0,0 +1,98 @@
+//Dom elemets
+const resultEl = document.getElementById("result");
+const lengthEl = document.getElementById("length");
+const uppercaseEl = document.getElementById("uppercase");
+const lowercaseEl = document.getElementById("lowercase");
+const numbersEl = document.getElementById("numbers");
+const symbolsEl = document.getElementById("symbols");
+const generateEl = document.getElementById("generate");
+const clipboardEl = document.getElementById("clipboard");
+
+
+generateEl.addEventListener('click', () => {
+    const length = +lengthEl.value;
+    const hasLower = lowercaseEl.checked;
+	const hasUpper = uppercaseEl.checked;
+	const hasNumber = numbersEl.checked;
+    const hasSymbol = symbolsEl.checked;
+    
+    resultEl.innerText = generatePassword(
+        hasLower,
+        hasUpper,
+        hasNumber,
+        hasSymbol,
+        length
+    );
+});
+
+//copy to clipboard
+clipboardEl.addEventListener('click', ()=>{
+    const textarea = document.createElement('textarea');
+    const password = resultEl.innerText;
+
+    if(!password){
+        return;
+    }
+    textarea.value = password;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy')
+    textarea.remove();
+    alert('Password Coppied to clipboard');
+});
+
+
+// generate password function
+function generatePassword(lower, upper, number, symbol, length) {
+
+    let generatePassword = '';
+
+    const typesCount = lower + upper + number +symbol;
+    console.log("count", typesCount);
+
+    const typesArr = [{lower}, {upper}, {number}, {symbol}].filter(item => Object.values(item)[0]);
+    console.log(typesArr);
+
+    if(typesArr === 0){
+        return '';
+    }
+
+    for(let i = 0; i < length; i += typesCount){
+        typesArr.forEach(type => {
+            const funcName = Object.keys(type)[0];
+            console.log('funcName', funcName);
+
+            generatePassword += randomFunc[funcName]();
+        });
+    }
+
+    const finalPassword = (generatePassword.slice(0,length));
+
+    return finalPassword
+}
+
+
+const randomFunc = {
+    lower: getRandomLover,
+    upper: getRandomUpper,
+    number: getRandomNumber,
+    symbol: getRandomSymbol,
+}
+
+// Generator functions
+function getRandomLover(){
+    return String.fromCharCode((Math.random() * 26) + 97)
+}
+
+function getRandomUpper(){
+    return String.fromCharCode((Math.random() * 26) + 65)
+}
+
+function getRandomNumber(){
+    return String.fromCharCode((Math.random() * 10) + 48)
+}
+
+function getRandomSymbol(){
+    const symbols = '!@#$%^&*(){}[]=<>/,.'
+    return  symbols[Math.floor(Math.random() * symbols.length)]
+}
